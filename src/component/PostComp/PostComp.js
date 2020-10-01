b728aa6b543807a1f10387dc138dcd75b293276e
Arif Budiman <arif.budiman8797@gmail.com>
2020-10-01 09:58:44 +0700

    selesai beljar crud

--- src/component/PostComp/PostComp.js
+++ src/component/PostComp/PostComp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Post = (props) => {
     return (
@@ -7,8 +8,10 @@ const Post = (props) => {
                 <img src="https://placeimg.com/640/480/tech" alt="post-img"/>
             </div>
             <div className="content">
-                <p className="title">{props.title}</p>
-                <p className="desc">{props.desc}</p>
+                <p className="title">{props.data.title}</p>
+                <p className="desc">{props.data.body}</p>
+                <button className="btn btn-success btn-sm" onClick={() => props.update(props.data)}>Ubah</button>
+                <button className="btn btn-danger btn-sm" onClick={() => props.remove(props.data.id)}>Hapus</button>
             </div>
         </div>
     )
--- src/container/Blog/BlogPost.js
+++ src/container/Blog/BlogPost.js
@@ -2,40 +2,119 @@ import React, {Component, Fragment} from 'react';
 import Post from '../../component/PostComp/PostComp.js';
 import './BlogPost.css';
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class BlogPost extends Component{
     state = {
-        post : []
+        post : [],
+        formBlogPost: {
+            id: 1,
+            userId: 1,
+            title: '',
+            body: '',
+        },
+        isUpdate: false
     }
-    // constructor(props) {
-    //     // super(props);
 
-    //     this.state = { name : "EventTestName" };
-    // }
+    getData = () => {
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
+        .then((res) => {
+            this.setState({
+                post: res.data
+            })
+        })
+    }
+
+    postData = () => {
+        axios.post('http://localhost:3004/posts', this.state.formBlogPost).then((res) => {
+            this.getData();
+            this.setState({
+                formBlogPost: {
+                    id: 1,
+                    userId: 1,
+                    title: '',
+                    body: '',
+                },
+            })
+        }, (err) => {
+            console.log('error', err);
+        })
+    }
+
+    putData = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost).then((res) => {
+            this.getData();
+            this.setState({
+                isUpdate: false,
+                formBlogPost: {
+                    id: 1,
+                    userId: 1,
+                    title: '',
+                    body: '',
+                },
+            })
+        }, (err) => {
+            console.log('error', err);
+        })
+    }
+
+    handleUpdate = (data) => {
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
+    }
+    
+    handleRemove = (data) => {
+        axios.delete(`http://localhost:3004/posts/${data}`).then((res) =>  
+            this.getData()
+        )
+    }
 
     componentDidMount(){
-        // axios.get('https://jsonplaceholder.typicode.com/posts')
-        // .then(function (response) {
-        //     this.setState({
-        //         post: response.data
-        //     })
-        //     // console.log(response.data)
-        // })
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(json => this.setState({
-                    post: json
-                }))
+        this.getData();
+    }
+
+    handleFormChange = (event) => {
+        let formBlogPostNew = {...this.state.formBlogPost};
+        let timestamp = new Date().getTime();
+        formBlogPostNew[event.target.name] = event.target.value;
+        if(!this.state.isUpdate){
+            formBlogPostNew['id'] = timestamp;
+        }
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+    }
+
+    handleFormSubmit = () => {
+        if(this.state.isUpdate){
+            this.putData();
+        }else{
+            this.postData();
+        }
     }
 
     render() {
-        
         return (
             <Fragment>
+                <div className="row">
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <label>Judul</label>
+                                <input type="text" name="title" className="form-control" value={this.state.formBlogPost.title} onChange={this.handleFormChange}/>
+                                <label>Body</label>
+                                <textarea name="body" className="form-control" value={this.state.formBlogPost.body} onChange={this.handleFormChange}/>
+                                <button className="btn btn-success" onClick={this.handleFormSubmit}>Simpan</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <p className="section-title">Recent Posts</p>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} title={post.title} desc={post.body}/>
+                        return <Post key={post.id} data={post} update={this.handleUpdate} remove={this.handleRemove}/>
                     })
                 }
             </Fragment>
